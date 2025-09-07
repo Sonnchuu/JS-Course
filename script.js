5613734c6c9cf311fd7c55125b23fa0683969973
Sony Stoyanov <soni.stoyanov@gmail.com>
2025-09-07 13:29:29 +0300

    challenge 2

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+// let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
 
-let firstName = 'Sony';
-console.log(firstName);
+// let firstName = 'Sony';
+// console.log(firstName);
+
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough){
+
+//     console.log('Sarah can start driving license 🚗');
+// }else{
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// let century;
+// const birth_year = 2012;
+// if(birth_year <= 2000){
+//     century = 20;
+// }else{
+//     century = 21;
+// }
+// console.log(century);
+
